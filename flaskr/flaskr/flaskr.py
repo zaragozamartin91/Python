0ce2b9189f81769488fa9541a49fad617a4b9dc6
Martin <zaragozamartin91@gmail.com>
2017-08-29 10:21:21 -0300

    Se trabajo hasta Step 6: The View Functions

--- flaskr/flaskr/flaskr.py
+++ flaskr/flaskr/flaskr.py
@@ -6,12 +6,15 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 app.config.update(dict(
-    DATABASE="flaskr"
+    DATABASE="flaskr",
+    USERNAME="root",
+    PASSWORD="root"
 ))
 
 def connect_db():
     "Abre una conexion con la BBDD."
     client = MongoClient()
+    # retorno un mapa con acceso a la bbdd y al cliente para manipular el cierre de sesion luego
     return {"db": client[app.config['DATABASE']], "client": client}
 
 # Flask provides two contexts: the application context and the request context
@@ -19,6 +22,7 @@ def connect_db():
 def get_db():
     """Opens a new database connection if there is none yet for the
     current application context."""
+    # hasattr verifica la existencia de un atributo en un diccionario
     if not hasattr(g,"mongo_db"):
         conn = connect_db()
         g.mongo_db = conn["db"]
@@ -36,3 +40,65 @@ def close_db(error):
         return
     if hasattr(g, "mongo_db"):
         g.mongo_client.close()
+
+def init_db():
+    "Inicializa la BBDD"
+    get_db()
+
+
+# The app.cli.command() decorator registers a new command with the flask script. When the command executes,
+# Flask will automatically create an application context which is bound to the right application. Within the function,
+# you can then access flask.g and other things as you might expect
+@app.cli.command('initdb')
+def initdb_command():
+    "Llama a la inicializacion de la BBDD"
+    init_db()
+    print("BBDD inicializada")
+
+@app.route('/')
+def show_entries():
+    """Busca las entradas en la BBDD y luego renderiza una vista
+    mostrando las entradas halladas"""
+    db = get_db()
+    coll = db.entries
+    entries = coll.find()
+    arr_entries = []
+    for entry in entries:
+        arr_entries.append(entry)
+    return render_template('show_entries.html', entries=arr_entries)
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    """Permite agregar una entrada"""
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    entry = {"title": request.form['title'], "text": request.form['text']}
+    coll = db.entries
+    entry_id = coll.insert_one(entry).inserted_id
+    print(entry_id)
+    flash("New entry was posted!")
+    return redirect(url_for('show_entries'))
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    """Permite obtener el formulario de inicio de sesion y
+    subir un formulario completado para iniciar sesion"""
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    """Cierra sesion de un usuario"""
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
--- flaskr/flaskr/schema.py
+++ flaskr/flaskr/schema.py
@@ -2,17 +2,29 @@
 
 from pymongo import MongoClient
 
-def main():
+def add():
     client = MongoClient()
     db = client.flaskr
 
-    entry = {"title": "Sample entry"}
+    entry = {"title": "Sample entry", "text":"Sample text"}
     entries = db.entries
     entry_id = entries.insert_one(entry).inserted_id
     
     print(entry_id)
     client.close()
 
+def find():
+    client = MongoClient()
+    db = client.flaskr
+    entries = db.entries
+    res = entries.find()
+    arr = []
+    for entry in res:
+        arr.append(entry)
+    print(arr)
+    # print(res)
+    client.close()
+
 if __name__ == "__main__":
-    main()
+    add()
 
